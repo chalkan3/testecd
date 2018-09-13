174c596150622a5bbc43d5f2adef990e83f41b42
chalkan33 <ighyss@googlemail.com>
2018-09-13 11:16:30 -0300

    sda
    sadsad

--- WebApplication1/Startup.cs
+++ WebApplication1/Startup.cs
@@ -18,11 +18,7 @@ namespace WebApplication1
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.Configure<ForwardedHeadersOptions>(options =>
-            {
-                options.ForwardedHeaders = 
-                    ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto;
-            });
+           
             services.AddResponseCompression();
             services.AddResponseCaching(options =>
             {
@@ -35,7 +31,7 @@ namespace WebApplication1
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            app.UseForwardedHeaders();
+           
 
             app.Run(async (context) =>
             {
