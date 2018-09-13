af4e82768b2dc61518897f000053abdbddb926a5
chalkan33 <ighyss@googlemail.com>
2018-09-13 10:48:31 -0300

    adsd
    adsdasd

--- WebApplication1/Startup.cs
+++ WebApplication1/Startup.cs
@@ -30,10 +30,10 @@ namespace WebApplication1
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+            app.UseForwardedHeaders(new ForwardedHeadersOptions
+{
+    ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+});
 
             app.Run(async (context) =>
             {
