71076a0f90f3693b24483a281c7aa763f32a962d
chalkan33 <ighyss@googlemail.com>
2018-09-13 09:22:24 -0300

    Kestrel
    Kestreel

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -19,6 +19,7 @@ namespace WebApplication1
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseKestrel()
                 .UseStartup<Startup>()
                 .UseUrls(urls: "http://localhost:3501");
     }
