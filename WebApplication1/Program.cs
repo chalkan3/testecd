b929026904005082116d0d34693422d76e86276f
chalkan33 <ighyss@googlemail.com>
2018-09-13 11:20:24 -0300

    asdd
    sadd

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -1,13 +1,18 @@
 using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 
 namespace WebApplication1
 {
     public class Program
     {
-          public static void Main(string[] args)
+        public static void Main(string[] args)
         {
             CreateWebHostBuilder(args).Build().Run();
         }
@@ -17,5 +22,4 @@ namespace WebApplication1
                 
                 .UseStartup<Startup>();
     }
-  
 }
